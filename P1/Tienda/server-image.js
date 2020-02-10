41d37c1b9ef7d7104b95a30db0f223008e61cb5b
Martinnello <ammenacho@hotmail.com>
2020-02-10 19:33:24 +0100

    Empezando tienda

--- P1/Tienda/server-image.js
+++ P1/Tienda/server-image.js
@@ -6,34 +6,31 @@ const PUERTO = 8080
 //-- Configurar y lanzar el servidor. Por cada peticion recibida
 //-- se imprime un mensaje en la consola
 http.createServer((req, res) => {
-  console.log("----------> Peticion recibida")
-  let q = url.parse(req.url, true);
-  console.log("Recurso:" + q.pathname)
+console.log("----------> Peticion recibida")
+let q = url.parse(req.url, true);
+console.log("Recurso:" + q.pathname.substr(1))
 
+let mime = ""
+let filename = ""
+var extens = q.pathname.split(".")[1]
 
-  let filename = ""
+if (q.pathname == "/"){
+  filename += "index.html"
+} else {
+  filename = q.pathname.substr(1)
+}
 
-  //-- Obtener fichero a devolver
-  if (q.pathname == "/")
-    filename += "index.html"
-
-  //-- Leer fichero
-  fs.readFile(filename, function(err, data) {
-
-    //-- Fichero no encontrado. Devolver mensaje de error
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
-
-    //-- Tipo mime por defecto: html
-    let mime = "text/html"
-
-    //-- Generar el mensaje de respuesta
-    res.writeHead(200, {'Content-Type': mime});
-    res.write(data);
-    res.end();
-  });
+fs.readFile(filename, (err, data) => {
+//-- Fichero no encontrado. Devolver mensaje de error
+if (err) {
+  res.writeHead(404, {'Content-Type': 'text/html'});
+  return res.end("404 Not Found");
+}
+//-- Generar el mensaje de respuesta
+res.writeHead(200, {'Content-Type': mime});
+res.write(data);
+res.end();
+});
 
 }).listen(PUERTO);
 
