627907a40f6e66a45f932c8430f2931ab1568c05
Martinnello <ammenacho@hotmail.com>
2020-04-13 16:19:22 +0200

    ok

--- Tienda/Practica_Tienda/Practica_Tienda/urls.py
+++ Tienda/Practica_Tienda/Practica_Tienda/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Japanese_Bits/', include('Japanese_Bits.urls')),
+    #path('Japanese_Bits/', include('Japanese_Bits.urls')),
 ]
