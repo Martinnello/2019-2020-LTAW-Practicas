b3435a02b9340f30bafd3a3ea944011b0fe88920
Martinnello <ammenacho@hotmail.com>
2020-05-16 14:01:13 +0200

    socket cliente conectado, establece conexion y desconexion y muestra mensaje de bienvenida

--- P4/Practica 4/Chat_AMM.js
+++ P4/Practica 4/Chat_AMM.js
@@ -7,44 +7,64 @@ const app = express()
 
 //-- Crear un servidor. Los mensajes recibidos
 //-- los gestiona la app
-const http = require('http').Server(app);
+const http = require('http').Server(app)
+
+//-- Biblioteca socket.io en el lado del servidor
+const io = require('socket.io')(http)
 
 //-- Puerto donde lanzar el servidor
 const PORT = 8080
 
 //-- Lanzar servidor
 http.listen(PORT, function(){
-  console.log('Servidor lanzado en puerto ' + PORT);
+  console.log('Servidor lanzado en puerto ' + PORT)
 });
 
 //-------- PUNTOS DE ENTRADA DE LA APLICACION WEB
 //-- Página principal
 app.get('/', (req, res) => {
-  let fich = __dirname + '/index.html';
-  res.sendFile(fich);
-  console.log("Acceso a " + fich);
-});
+  let fich = __dirname + '/Chat.html'
+  res.sendFile(fich)
+  console.log("Acceso a " + fich)
+})
 
 //-- Vista Help
 app.get('/help', (req, res) => {
   res.send('Comandos: <br><br> - /help: Ayuda. <br> - /list: Lista de usuarios conectados. <br> - /hello: Devuelve un saludo. <br> - /date: Fecha Actual.');
-  console.log("Acceso a /help");
-});
+  console.log("Acceso a /help")
+})
 
 //-- Vista List
 app.get('/list', (req, res) => {
   res.send('- /list: Lista de usuarios conectados.');
-  console.log("Acceso a /list");
+  console.log("Acceso a /list")
 });
 
 //-- Vista hello
 app.get('/hello', (req, res) => {
   res.send('Bienvenido al chat de AMM!')
-  console.log("Acceso a /hello");
-});
+  console.log("Acceso a /hello")
+})
 
 //-- Vista date
 app.get('/date', (req, res) => {
   res.send('Fecha de hoy!')
-  console.log("Acceso a /date");
+  console.log("Acceso a /date")
 });
+
+//-- El resto de peticiones se interpretan como
+//-- ficheros estáticos
+app.use('/', express.static(__dirname +'/'))
+
+//-- Evento: Nueva conexion recibida
+//-- Un nuevo cliente se ha conectado!
+io.on('connection', function(socket){
+
+  //-- Usuario conectado. Imprimir el identificador de su socket
+  console.log('--> Usuario conectado!. Socket id: ' + socket.id)
+  socket.emit('hello', "Bienvenido al Chat")
+  //-- Usuario desconectado. Imprimir el identificador de su socket
+  socket.on('disconnect', function(){
+  console.log('--> Usuario Desconectado. Socket id: ' + socket.id)
+  })
+})
--- P4/Practica 4/Chat_client_socket.js
+++ P4/Practica 4/Chat_client_socket.js
@@ -1,15 +1,9 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>CHAT 1</title>
-    <script src="/socket.io/socket.io.js"></script>
-    <script src="chat-1-client.js" defer></script>
-  </head>
-  <body>
-    <p>Plantilla para mi chat....</p>
-    <input id="msg" autocomplete="off" />
-    <button type="button" id="send">SEND</button>
-    <p id="display"></p>
-  </body>
-</html>
+console.log("Ejecutando cliente JS...");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+//-- Se ha recibido el evento 'hello':
+//-- Es el mensaje de bienvenida del servidor
+socket.on('hello', (msg) => {
+  console.log("Mensaje del servidor: " + msg);
+});
