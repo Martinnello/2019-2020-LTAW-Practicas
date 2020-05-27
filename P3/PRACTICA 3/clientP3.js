45e3a6c1c28dea87e7f56641ee0e3b019aa87fdb
Martinnello <ammenacho@hotmail.com>
2020-05-27 22:50:26 +0200

    quitado parametro 2 no necesario de momento y cliente depurado

--- P3/PRACTICA 3/clientP3.js
+++ P3/PRACTICA 3/clientP3.js
@@ -4,17 +4,15 @@ const ver = document.getElementById('ver')
 //-- Obtener el párrafo del DOM donde mostrar el resultado
 const resultado = document.getElementById('resultado');
 
-//-- Cuando el usuario aprieta el botón de ver los productos
+//-- Cuando el usuario escribe ver los productos (+ de 3 letras)
 ver.onkeyup = ()=>{
 
   //-- Crear objeto para hacer peticiones AJAX
   const m = new XMLHttpRequest();
 
   //-- Configurar la petición
-  m.open("GET","http://localhost:8080/myquery?param1="+ver.value+"&param2=wei", true);
+  m.open("GET","http://localhost:8080/myquery?param1="+ver.value, true);
 
-  //-- Cuando la haya alguna noticia sobre la peticion
-  //-- ejecuta este código
   m.onreadystatechange=function(){
      //-- Petición enviada y recibida. Todo OK!
      if (m.readyState==4 && m.status==200){
@@ -22,20 +20,15 @@ ver.onkeyup = ()=>{
        //-- La respuesta es un objeto JSON
        let productos = JSON.parse(m.responseText)
 
-       //-- Borrar el resultado anterior que hubiese en el párrafo
-       //-- de resultado
+       //-- Borrar el resultado anterior
        resultado.innerHTML = "";
 
-       //--Recorrer los productos del objeto JSON
+       //-- Recorrer los productos del objeto JSON
        for (let i=0; i < productos.length; i++) {
-
-         //-- Añadir cada producto al párrafo de visualización
          resultado.innerHTML += productos[i];
-
        }
      }
    }
-
    //-- Enviar la petición!
    m.send();
 }
