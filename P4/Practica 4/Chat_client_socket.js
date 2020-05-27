6e5c68623811c43df4fb3dbef841a3307aa454ee
Martinnello <ammenacho@hotmail.com>
2020-05-27 22:56:08 +0200

    cliente p4 depurado

--- P4/Practica 4/Chat_client_socket.js
+++ P4/Practica 4/Chat_client_socket.js
@@ -11,28 +11,28 @@ const send = document.getElementById("send")
 //-- Crear un websocket. Se establece la conexión con el servidor
 const socket = io()
 
-socket.emit('new', nick);
+socket.emit('new', nick)
 
-console.log(nick + " se ha conectado");
+console.log(nick + " se ha conectado")
 
 //-- Se ha recibido un mensaje "new"
 socket.on('new', (msg) => {
   display.innerHTML += "<br> > " + msg
-});
+})
 
 //-- Se ha recibido un mensaje de chat
 socket.on('msg', (msg) => {
   display.innerHTML += "<br> > " + msg
-});
+})
 
 //-- Botón de envío
 send.onclick = () => {
   if (msg.value){
     if (msg.value[0] == '/'){
-      socket.emit('cmd', msg.value);
+      socket.emit('cmd', msg.value)
     }else {
-      socket.emit('msg', msg.value);
+      socket.emit('msg', msg.value)
     }
   }
-  msg.value="";
+  msg.value=""
 }
