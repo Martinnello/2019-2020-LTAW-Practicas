08d465befe45bdb5e4c903d676b0da4db016f56f
Martinnello <ammenacho@hotmail.com>
2020-06-02 11:59:17 +0200

    Nombre de la tienda cambiado, ficheros actualizados y css adaptado

--- P2/PRACTICA 2/Practica_Tienda/Japanese_Bits/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductosJaponesesConfig(AppConfig):
-    name = 'Productos_Japoneses'
---
+++ P2/PRACTICA 2/Proyecto_de_tienda_PS4/PS4_Games_Store/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Ps4GamesStoreConfig(AppConfig):
+    name = 'PS4_Games_Store'
